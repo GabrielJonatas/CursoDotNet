cf55f9336debe624ec6468b1e3037930b51b0e86
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2024-05-10 11:51:11 -0300

    feat: IDE and language basics

---
+++ EstruturaProjeto/Program.cs
@@ -0,0 +1,42 @@
+﻿using EstruturaProjeto.Models;
+
+int a = Convert.ToInt32("5"); // cast - Casting de valor string para int
+
+Console.WriteLine(a);
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c += 5; // c = c + 5;
+
+// Console.WriteLine(c);
+
+// DateTime dataAtual = DateTime.Now;
+
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura);
+// Console.WriteLine("Valor da variável preço: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Buta";
+// pessoa1.Idade = 26;
+
+// pessoa1.Apresentar();
